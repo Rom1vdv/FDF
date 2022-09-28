3d539b51da2faaec5369b294e5684faf9dd9ca03
Romain Van der Vennet <romain.vennet@outlook.com>
2022-09-28 18:40:03 +0200

    owo

--- source/fdf_mapping.c
+++ source/fdf_mapping.c
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:44:54 by romvan-d          #+#    #+#             */
-/*   Updated: 2022/09/25 21:13:56 by romvan-d         ###   ########.fr       */
+/*   Updated: 2022/09/28 18:39:40 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ void    link_map_points(t_image_data *img, t_fdf_map *map)
     size_t column;
     
     column = 0;
+	printf("%zu-%zu\n", map->column_len, map->row_len);
     while(column < map->column_len)
     {
         row = 0;
         while(row < map->row_len)
         {
-            ft_draw_line(img, (t_coordinates){*map->parsed_map[row], *map->parsed_map[column]}, (t_coordinates){*map->parsed_map[row + 1], *map->parsed_map[column + 1]});
+            ft_draw_line(img, (t_coordinates){(row * 50) + 200, column * 50}, (t_coordinates){(row * 50) + 200, (column + 1 )* 50});
+			ft_draw_line(img, (t_coordinates){(row * 50) + 200, column * 50}, (t_coordinates){((row + 1) * 50) + 200, column * 50});
             row++;
         }
         column++;
--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 14:46:23 by romvan-d          #+#    #+#             */
-/*   Updated: 2022/09/25 20:44:19 by romvan-d         ###   ########.fr       */
+/*   Updated: 2022/09/28 11:36:03 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,13 @@ int main(int argc, char **argv)
             parsing_list = ft_convert_map_to_list(fd);
             map = ft_create_parsed_map(parsing_list);
             mlx = mlx_init();
-            mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World");
+            mlx_win = mlx_new_window(mlx, 1920, 1080, "Fdf");
             img.img = mlx_new_image(mlx, 1920, 1080);
             img.address= mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
             link_map_points(&img, &map);
             mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
             mlx_loop(mlx);
-            // printf("This cell equals to : %d\n", map.parsed_map[0][0] );
+            printf("This cell equals to : %d\n", map.parsed_map[11][11] );
             
         }
     }
