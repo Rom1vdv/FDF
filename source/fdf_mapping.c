c48f9f41823a0b4bf1c31b61d96eae489ffacbe5
Romain Van der Vennet <romain.vennet@outlook.com>
2022-09-29 12:28:51 +0200

    owo

--- source/fdf_mapping.c
+++ source/fdf_mapping.c
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:44:54 by romvan-d          #+#    #+#             */
-/*   Updated: 2022/09/28 18:39:40 by romvan-d         ###   ########.fr       */
+/*   Updated: 2022/09/28 19:28:27 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ void    link_map_points(t_image_data *img, t_fdf_map *map)
 {
     size_t row;
     size_t column;
-    
+    int z_component;
+	
     column = 0;
 	printf("%zu-%zu\n", map->column_len, map->row_len);
     while(column < map->column_len)
     {
         row = 0;
         while(row < map->row_len)
-        {
-            ft_draw_line(img, (t_coordinates){(row * 50) + 200, column * 50}, (t_coordinates){(row * 50) + 200, (column + 1 )* 50});
-			ft_draw_line(img, (t_coordinates){(row * 50) + 200, column * 50}, (t_coordinates){((row + 1) * 50) + 200, column * 50});
+        {	
+			if (column < map->column_len - 1)
+            	ft_draw_line(img, (t_coordinates){(row * 50) + 800, column * 50}, (t_coordinates){(row * 50) + 800, (column + 1)* 50});
+			if (row < map->column_len - 1)
+				ft_draw_line(img, (t_coordinates){(row * 50) + 800, column * 50}, (t_coordinates){((row + 1) * 50) + 800, column * 50});
             row++;
         }
         column++;
